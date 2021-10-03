2898cb837d2de49796a3656a262eb916690cc2b1
KimJaeHwan <rfkjh22@naver.com>
2021-10-03 19:38:26 +0900

    palm identification

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ Created on Sat Oct  2 19:27:12 2021
 
 import cv2
 import mediapipe as mp
+import math
+import numpy as np
+from google.protobuf.json_format import MessageToDict
+
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -18,8 +23,35 @@ def comp_int(num1, num2):
     else :
         return int(num2), int(num1)
     
+    
+def len_line(coord):
+    return np.sqrt(np.sum(coord*coord))
+
+def isPalm(coord, hand_):
+    mov_zero = coord - coord[0]
+    len_l = len_line(mov_zero[2])
+    sin = -(mov_zero[2][1]/len_l)
+    cos = mov_zero[2][0]/len_l
+    y2 = coord[1][0]*sin + coord[1][1]*cos
+    y17 = coord[3][0]*sin + coord[3][1]*cos
+    
+    print(hand_)
+    if (y2 < y17):          # 엄지가 아래
+        if (hand_ == 0):    # 왼손인 경우
+            return 0
+        else :              # 오른손인 경우
+            return 1
+    else :                  # 엄지가 위쪽
+        if (hand_ == 0):    # 왼손인 경우
+            return 1
+        else :              # 오른손인 경우
+            return 0
+    return 0
+
+finger_tip = [3,7,11,15,19]
+palm_point = [0,2 ,9, 17]
 # For static images:
-IMAGE_FILES = ["./hand2_img.jpg"]
+IMAGE_FILES = ["./hand_img2.jpg","./hand_img3.jpg","./hand_img4.jpg","./hand_img5.jpg","./hand_img6.jpg","./hand2_img.jpg",]
 with mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=2,
@@ -37,10 +69,14 @@ with mp_hands.Hands(
       continue
     image_height, image_width, _ = image.shape
     annotated_image = image.copy()
+    img_test = image.copy()
+    
+    hand_cnt = 0
     for hand_landmarks in results.multi_hand_landmarks:
+      '''
       print('hand_landmarks:', hand_landmarks)
+      
       print(type(mp_hands.HandLandmark.INDEX_FINGER_TIP))
-      print(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP])
       print(
           f'Index finger tip coordinates: (',
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
@@ -62,31 +98,39 @@ with mp_hands.Hands(
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].x * image_width}, '
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y * image_height})'
       )
-      img_test = image.copy()
-      idx_ = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].x * image_width
-      idy_ = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y * image_height
-      itx_ = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width
-      ity_ = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height
-      color = ( 255,0,0)
       '''
-      if(idx_ < itx_):
-          x1 = int(idx_)
-          x2 = int(itx_)
-      else:
-          x1 = int(itx_)
-          x2 = int(idx_)
+      # detecting finter_tips 
+      for i in finger_tip:
+          idx_ = hand_landmarks.landmark[i + 1].x * image_width
+          idy_ = hand_landmarks.landmark[i + 1].y * image_height
+          itx_ = hand_landmarks.landmark[i].x * image_width
+          ity_ = hand_landmarks.landmark[i].y * image_height
+          color = ( 255,0,0)
+          img_test = cv2.line(img_test,(int(idx_),int(idy_)),(int(itx_),int(ity_)),color,5)
+          
+      # detectng palm
+      palm_point_coordinate = [];
+      for i in palm_point:
+          temp = []
+          temp.append(hand_landmarks.landmark[i].x * image_width)
+          temp.append(hand_landmarks.landmark[i].y * image_height)
+          palm_point_coordinate.append(temp)
+          
+      hand_ = MessageToDict(results.multi_handedness[hand_cnt])['classification'][0]['index']
       
-      if(idy_ < ity_):
-          y1 = int(idy_)
-          y2 = int(ity_)
-      else:
-          y1 = int(ity_)
-          y2 = int(idy_)
-      '''
+      hand_cnt += 1
+      print(palm_point_coordinate)
       
-      
-      img_test = cv2.line(img_test,(int(idx_),int(idy_)),(int(itx_),int(ity_)),color,5)
-     
+      front = isPalm(np.array(palm_point_coordinate), hand_)
+      if(front == 0):       # 손바닥 앞면 구분
+          color = (255,102,165)
+      else :
+          color = (61,61,204)
+          
+      img_test = cv2.line(img_test,(int(palm_point_coordinate[0][0]),int(palm_point_coordinate[0][1])),(int(palm_point_coordinate[1][0]),int(palm_point_coordinate[1][1])),color, 5)
+      img_test = cv2.line(img_test,(int(palm_point_coordinate[2][0]),int(palm_point_coordinate[2][1])),(int(palm_point_coordinate[1][0]),int(palm_point_coordinate[1][1])),color, 5)
+      img_test = cv2.line(img_test,(int(palm_point_coordinate[3][0]),int(palm_point_coordinate[3][1])),(int(palm_point_coordinate[0][0]),int(palm_point_coordinate[0][1])),color, 5)
+      img_test = cv2.line(img_test,(int(palm_point_coordinate[3][0]),int(palm_point_coordinate[3][1])),(int(palm_point_coordinate[2][0]),int(palm_point_coordinate[2][1])),color, 5)
       
       cv2.imshow('test',img_test)
       cv2.waitKey(0)
@@ -98,11 +142,12 @@ with mp_hands.Hands(
           mp_hands.HAND_CONNECTIONS,
           mp_drawing_styles.get_default_hand_landmarks_style(),
           mp_drawing_styles.get_default_hand_connections_style())
-      
-      
+
+    '''
     cv2.imwrite(
         './' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    '''
     cv2.imwrite(
-        './' + str(idx)+'_' + '.png', cv2.flip(img_test, 1))
+        './img_res/' + str(idx)+'_' + '.png', cv2.flip(img_test, 1))
     
     
